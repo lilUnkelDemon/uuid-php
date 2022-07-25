add0631d43da6a3fd44e08006b0a08afe9f6856a
Amirabbas Momeni <amir.momeni.gelloria79@gmail.com>
2022-07-25 14:39:32 +0430

    upload project = on GitHub

---
+++ uuid-func.php
@@ -0,0 +1,33 @@
+<?php
+function php_UUID($name_space, $string) {
+    $n_hex = str_replace(array('-','{','}'), '', $name_space); // Getting hexadecimal components of namespace
+    $binary_str = ''; // Binary Value
+
+	//Namespace UUID to bits conversion
+    for($i = 0; $i < strlen($n_hex); $i+=2) {
+      $binary_str .= chr(hexdec($n_hex[$i].$n_hex[$i+1]));
+    }
+    //hash value
+    $hashing = md5($binary_str . $string);
+
+    return sprintf('%08s-%04s-%04x-%04x-%12s',
+      // 32 bits for the time low
+      substr($hashing, 0, 8),
+      // 16 bits for the time mid
+      substr($hashing, 8, 4),
+      // 16 bits for the time hi,
+      (hexdec(substr($hashing, 12, 4)) & 0x0fff) | 0x3000,
+      // 8 bits and 16 bits for the clk_seq_hi_res,
+      // 8 bits for the clk_seq_low,
+      (hexdec(substr($hashing, 16, 4)) & 0x3fff) | 0x8000,
+      // 48 bits for the node
+      substr($hashing, 20, 12)
+    );
+  }
+
+$php_UUID = php_UUID('5f6384bfec4ca0b2d4114a13aa2a5435', 'delftstack!');
+echo $php_UUID;
+$php_UUID = php_UUID('591531f16f581b69a390980eb282ba83', 'this is delftstack!');
+echo "<br>";
+echo $php_UUID;
+?>
